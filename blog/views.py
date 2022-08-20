91893f7c2f9e0e13a3947f85eff1d90fb5c23186
evgen4ikrus <67272160+evgen4ikrus@users.noreply.github.com>
2022-08-20 11:19:30 +0300

    reduce db queries number

--- blog/views.py
+++ blog/views.py
@@ -7,27 +7,6 @@ def get_related_posts_count(tag):
     return tag.posts.count()
 
 
-def serialize_post(post):
-    return {
-        'title': post.title,
-        'teaser_text': post.text[:200],
-        'author': post.author.username,
-        'comments_amount': len(Comment.objects.filter(post=post)),
-        'image_url': post.image.url if post.image else None,
-        'published_at': post.published_at,
-        'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.all()],
-        'first_tag_title': post.tags.all()[0].title,
-    }
-
-
-def serialize_tag(tag):
-    return {
-        'title': tag.title,
-        'posts_with_tag': len(Post.objects.filter(tags=tag)),
-    }
-
-
 def serialize_post_optimized(post):
     return {
         'title': post.title,
@@ -37,7 +16,7 @@ def serialize_post_optimized(post):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'tags': [serialize_tag(tag) for tag in post.tags.annotate(num_posts=Count('posts'))],
         'first_tag_title': post.tags.all()[0].title,
     }
 
@@ -45,7 +24,7 @@ def serialize_post_optimized(post):
 def serialize_tag(tag):
     return {
         'title': tag.title,
-        'posts_with_tag': len(Post.objects.filter(tags=tag)),
+        'posts_with_tag': tag.num_posts,
     }
 
 
@@ -87,7 +66,7 @@ def post_detail(request, slug):
 
     likes = post.likes.all()
 
-    related_tags = post.tags.all()
+    related_tags = post.tags.annotate(num_posts=Count('posts'))
 
     serialized_post = {
         'title': post.title,
